ac6dc5268716eba125f5f7f42e7ee5b37946e335
Caleb <ca30850@gmail.com>
2024-05-21 20:07:08 -0500

    create componenetes Msg, TextColor

---
+++ src/components/vue/TextColor.vue
@@ -0,0 +1,12 @@
+<script setup>
+let ArrayWords = ["Hola","Mundo","cruel",":D"];
+
+</script>
+
+<template>
+
+<ul class="w-[100px] h-auto ">
+    <li v-for="(word,index) in ArrayWords" :key="index" :class="{'bg-green-600' : (index %2  == 0 )} " class="text-white">{{word}}</li>
+</ul>
+
+</template>
